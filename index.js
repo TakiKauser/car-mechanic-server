e8d7cdc6851576e8fc589637f8bbd669ca6977ce
TakiKauser <takikauserbadhon@gmail.com>
2021-10-25 01:31:20 +0600

    secured-info

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// user-pass secured using "dotenv(install)"
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sydng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', (req, res) => {
     res.send("Car Mechanics");
